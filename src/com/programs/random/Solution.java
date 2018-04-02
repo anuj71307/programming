a095903f5e1c9a1d34b1594fec4c840181d8f9b2
ajha <anujjha1@in.ibm.com>
2018-04-02 20:26:16 +0530

    Reverse queue using recursion

--- src/com/programs/random/Solution.java
+++ src/com/programs/random/Solution.java
@@ -1,5 +1,9 @@
 package com.programs.random;
 
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * class which will call appropriate method and classes
  * for different problems
@@ -8,9 +12,37 @@ package com.programs.random;
  */
 public class Solution {
 
-	
+
+    /**
+     * Reverse a queue using recursion
+     * @param queue to reverse
+     * @return reverse queue
+     */
+	public static Queue<Integer> reverseQueue(Queue<Integer> queue){
+
+		if(queue.isEmpty()){
+			return queue;
+		}
+		Integer element  = queue.poll();
+		reverseQueue(queue);
+
+		queue.add(element);
+		return queue;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(12);
+		queue.add(13);
+		queue.add(14);
+		queue.add(15);
+
+		queue = reverseQueue(queue);
+
+		while(!queue.isEmpty()){
+			System.out.println(queue.poll());
+		}
 
 	}
 
