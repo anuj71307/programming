ae36ee298e726ff5beb8e71f9f9cff86bfe91fb6
anuj-dialpad <ajha@dialpad.com>
2019-08-19 05:17:58 +0530

    Problem: Generate Unique permutations of a number

--- src/com/programs/problems/ArrayProblems.java
+++ src/com/programs/problems/ArrayProblems.java
@@ -160,9 +160,41 @@ public class ArrayProblems {
 
     public static void main(String[] args) {
 
-        int arr[] = new int[]{1,3,1,2,0,5};
-        System.out.print(maxWaterTrap(arr));
+        int arr[] = new int[]{1,1};
+        System.out.print(permuteUnique(arr));
     }
+
+    /**
+     * https://leetcode.com/problems/permutations-ii/
+     * generate all unique permutation of a number
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
+        return list;
+    }
+
+
+    private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean[] used){
+        if(tempList.size()==nums.length){
+            list.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for(int i =0; i< nums.length;i++){
+            if(used[i] ||(i>0 && nums[i]== nums[i-1] && !used[i-1]) ) continue;
+            used[i] = true;
+            tempList.add(nums[i]);
+            backtrack(list, tempList,nums,used);
+            used[i] = false;
+            tempList.remove(tempList.size() - 1);
+        }
+
+    }
+
 
     /**
      * https://leetcode.com/problems/container-with-most-water/
