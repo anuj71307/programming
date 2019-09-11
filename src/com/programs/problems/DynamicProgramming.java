9b8e2cea01f4ab57f31886dd7bb2fea96389b2a5
anuj-dialpad <ajha@dialpad.com>
2019-09-11 22:01:28 +0530

    Problem: Coin Change

--- src/com/programs/problems/DynamicProgramming.java
+++ src/com/programs/problems/DynamicProgramming.java
@@ -17,6 +17,36 @@ public class DynamicProgramming {
     }
 
     /**
+     * https://leetcode.com/problems/coin-change/
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange(int[] coins, int amount) {
+        if(amount<1) return 0;
+        return coinChange(coins, amount, new int [amount]);
+
+    }
+
+    public int coinChange(int[] coins, int amount, int [] count) {
+        if(amount<0) return -1;
+        if(amount==0) return 0;
+        if(count[amount-1]!=0) return count[amount-1];
+        int min = Integer.MAX_VALUE;
+
+        for(int coin: coins){
+            int res = coinChange(coins, amount-coin, count);
+            if(res>=0 && res<min){
+                min = 1+res;
+            }
+        }
+
+        count[amount-1] = min==Integer.MAX_VALUE?-1:min;
+        return count[amount-1];
+
+    }
+
+    /**
      * https://leetcode.com/problems/partition-equal-subset-sum/
      * @param nums
      * @return
