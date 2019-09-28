edf89d76b4f471daaea7ce34739818862f55ad85
Anuj Kumar Jha <5445235+anuj-github@users.noreply.github.com>
2019-09-28 13:19:50 +0530

    Problems: Reduce to one (#84)

--- src/com/programs/problems/DynamicProgramming.java
+++ src/com/programs/problems/DynamicProgramming.java
@@ -13,7 +13,32 @@ public class DynamicProgramming {
         int target = 7168;
 
         DynamicProgramming dp = new DynamicProgramming();
-        System.out.println(dp.numSquares(target));
+        System.out.println(dp.reduceToOne(target));
+    }
+
+    /**
+     * On a positive integer, you can perform any one of the following 3 steps.
+     * 1.) Subtract 1 from it. ( n = n - 1 )  ,
+     * 2.) If its divisible by 2, divide by 2. ( if n % 2 == 0 , then n = n / 2  )  ,
+     * 3.) If its divisible by 3, divide by 3. ( if n % 3 == 0 , then n = n / 3  ).
+     * Now the question is, given a positive integer n, find the minimum number of steps that takes n to 1
+     *
+     * eg: 1.)For n = 1 , output: 0
+     * 2.) For n = 4 , output: 2  ( 4  /2 = 2  /2 = 1 )
+     * 3.)  For n = 7 , output: 3  (  7  -1 = 6   /3 = 2   /2 = 1 )
+     * @param num
+     * @return
+     */
+    private int reduceToOne(int num){
+        int res [] = new int[num+1];
+        res[2] = 1;
+        for(int i =3;i<=num;i++){
+            int min = 1+res[i-1];
+            if(i%2==0) min = Math.min(min, 1+res[i/2]);
+            if(i%3==0) min = Math.min(min, 1+res[i/3]);
+            res[i] = min;
+        }
+        return res[num];
     }
 
     /**
