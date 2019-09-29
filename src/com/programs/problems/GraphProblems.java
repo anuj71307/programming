c3a892d6a9a045391afb2ada366f18bd0ff7ba3e
Anuj Kumar Jha <5445235+anuj-github@users.noreply.github.com>
2019-09-29 10:12:31 +0530

    Problem: True Frequency (#86)

--- src/com/programs/problems/GraphProblems.java
+++ src/com/programs/problems/GraphProblems.java
@@ -1,9 +1,8 @@
 package com.programs.problems;
 
-import com.programs.graph.Graph;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class GraphProblems {
 
@@ -17,15 +16,15 @@ public class GraphProblems {
             String arr[] = str.split(" ");
             int v = Integer.parseInt(arr[0]);
             int e = Integer.parseInt(arr[1]);
-            Graph graph = new Graph(v, false);
+            //Graph graph = new Graph(v, false);
             for (int i = 0; i < e; i++) {
                 String str1 = reader.readLine();
                 String arr2[] = str1.split(" ");
                 int s = Integer.parseInt(arr2[0]);
                 int d = Integer.parseInt(arr2[1]);
-                graph.addEdge(s, d);
+                // graph.addEdge(s, d);
             }
-            graph.print();
+            //graph.print();
 
         }
         reader.close();
@@ -33,6 +32,104 @@ public class GraphProblems {
 
 
     public static void main(String[] args) {
+
+        HashMap<String, Integer> nameCount = new HashMap<>();
+        nameCount.put("Jon", 10);
+        nameCount.put("John", 2);
+        nameCount.put("Johny", 3);
+        nameCount.put("Karl", 4);
+        nameCount.put("Carleton", 2);
+        nameCount.put("Max", 3);
+        HashMap<String, String> synonyms = new HashMap<>();
+        synonyms.put("John", "Jon");
+        synonyms.put("Johny", "John");
+        synonyms.put("Karl", "Carleton");
+        synonyms.put("Johny", "John");
+
+        nameCount = new GraphProblems().gettrueFrequency(nameCount, synonyms);
+        for (Map.Entry<String, Integer> entry : nameCount.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+    }
+
+    private HashMap<String, Integer> gettrueFrequency(HashMap<String, Integer> nameCount, HashMap<String, String> synonyms) {
+
+        HashMap<String, Integer> res = new HashMap<>();
+
+        Graph graph = buildGraph(nameCount, synonyms);
+
+        GraphNode node = null;
+        for (Map.Entry<String, GraphNode> entry : graph.map.entrySet()) {
+            node = entry.getValue();
+            if (!node.isVisited) {
+                int count = traverse(node);
+                res.put(node.name, count);
+            }
+        }
+        return res;
+    }
+
+    private int traverse(GraphNode node) {
+
+        node.isVisited = true;
+        int count = node.count;
+        for (GraphNode child : node.children) {
+            if (!child.isVisited) {
+                child.isVisited = true;
+                count += traverse(child);
+            }
+        }
+
+        return count;
+    }
+
+    private Graph buildGraph(HashMap<String, Integer> nameCount, HashMap<String, String> synonyms) {
+        Graph graph = new Graph();
+        for (Map.Entry<String, Integer> entry : nameCount.entrySet()) {
+            graph.createNode(entry.getKey(), entry.getValue());
+        }
+
+        for (Map.Entry<String, String> entry : synonyms.entrySet()) {
+            graph.addEdge(entry.getKey(), entry.getValue());
+        }
+
+        return graph;
+    }
+
+    class Graph {
+
+        HashMap<String, GraphNode> map;
+
+        Graph() {
+            map = new HashMap<>();
+        }
+
+        void createNode(String name, int count) {
+            map.put(name, new GraphNode(name, count));
+        }
+
+        public void addEdge(String name1, String name2) {
+            GraphNode first = map.get(name1);
+            GraphNode second = map.get(name2);
+            if (first == null || second == null) return;
+            first.children.add(second);
+            second.children.add(first);
+        }
+    }
+
+
+    class GraphNode {
+        String name;
+        int count;
+        List<GraphNode> children;
+        boolean isVisited;
+
+        GraphNode(String name, int count) {
+            this.name = name;
+            this.count = count;
+            children = new LinkedList<>();
+        }
     }
 
     /* https://leetcode.com/problems/clone-graph/
