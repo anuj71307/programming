87a259b1606d1443bebe06c4dde3cc7c62451404
Anuj Kumar Jha <5445235+anuj-github@users.noreply.github.com>
2019-10-03 23:01:44 +0530

    Problems: Maximal Square (#91)

--- src/com/programs/problems/DynamicProgramming.java
+++ src/com/programs/problems/DynamicProgramming.java
@@ -19,6 +19,38 @@ public class DynamicProgramming {
     }
 
     /**
+     * https://leetcode.com/problems/maximal-square/
+     * @param matrix
+     * @return
+     */
+    public int maximalSquare(char[][] matrix) {
+        if(matrix==null || matrix.length==0) return 0;
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int dp[][] = new int[r+1][c+1];
+        int max = 0;
+        for(int i =1; i<=r;i++){
+            for(int j =1;j<=c;j++){
+                if(matrix[i-1][j-1]=='1'){
+                    dp[i][j] = getMin(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) +1;
+                    max = Math.max(max,dp[i][j]);
+                }
+            }
+
+        }
+        return max*max;
+    }
+
+
+    int getMin(int x, int y, int z){
+
+        int min = x;
+        if(y<min) min =y;
+        if(z<min) min = z;
+        return min;
+    }
+
+    /**
      * DP Solution
      * https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
      *
