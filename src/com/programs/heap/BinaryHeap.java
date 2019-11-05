f03c5b1055e0591914c1238be63b287d51630a30
Anuj Kumar Jha <5445235+anuj-github@users.noreply.github.com>
2019-11-05 09:51:31 +0530

    Problems: Max Heap (#132)

---
+++ src/com/programs/heap/BinaryHeap.java
@@ -0,0 +1,57 @@
+package com.programs.heap;
+
+public abstract  class BinaryHeap {
+
+    int arr[];
+    int size;
+    int capacitiy;
+
+    BinaryHeap(int capacitiy){
+        this.capacitiy = capacitiy;
+        arr = new int[capacitiy];
+    }
+
+    BinaryHeap(int arr[]){
+        this.arr = arr;
+        capacitiy = arr.length;
+        size = capacitiy;
+
+        for(int i = arr.length/2;i>=0;i--){
+            heapify(i, capacitiy);
+        }
+    }
+
+    void swap(int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    int getLeft(int index){
+        return 2*index+1;
+    }
+    int getRight(int index){
+        return 2*index+2;
+    }
+    int getParent(int index){
+        return (index-1)/2;
+    }
+    int getMin() {
+        throw new RuntimeException("Not valid op");
+    }
+    int getMax() {
+        throw new RuntimeException("Not valid op");
+    }
+    public abstract void insert(int value);
+    public abstract void delete(int value);
+    public abstract void heapify();
+    public abstract int[] sortArray();
+    public abstract void heapify(int index, int length);
+    int extractMin(){
+       throw new RuntimeException("Not valid op");
+    }
+    int extractMax(){
+
+        throw new RuntimeException("Not valid op");
+    }
+}
