fdaae45ca1294494c0870890df6f03a8b7678aee
anuj-dialpad <ajha@dialpad.com>
2019-11-26 18:44:57 +0530

    Problems: Range Sum BST

--- src/com/programs/problems/TreeProblems.java
+++ src/com/programs/problems/TreeProblems.java
@@ -23,6 +23,28 @@ public class TreeProblems {
     }
 
     /**
+     * https://leetcode.com/problems/range-sum-of-bst/
+     *
+     * @param root
+     * @param L
+     * @param R
+     * @return
+     */
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) return 0;
+        int sum = 0;
+        if (root.val >= L && root.val <= R) {
+            sum += root.val;
+            return sum + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+        }
+
+        if (root.val < L) {
+            return sum + rangeSumBST(root.right, L, R);
+        }
+        return sum + rangeSumBST(root.left, L, R);
+    }
+
+    /**
      * https://www.geeksforgeeks.org/find-closest-element-binary-search-tree/
      *
      * @param root
