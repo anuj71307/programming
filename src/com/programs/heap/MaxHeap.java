13f121b5eb91a56309b78a9cc0e02b4f7ef21972
Anuj Kumar Jha <5445235+anuj-github@users.noreply.github.com>
2019-12-06 12:11:53 +0530

    Problems: Comment (#145)

--- src/com/programs/heap/MaxHeap.java
+++ src/com/programs/heap/MaxHeap.java
@@ -1,5 +1,9 @@
 package com.programs.heap;
 
+/**
+ * This class should be used to create max heap of a array.
+ * Currently it support integer data only.
+ */
 public class MaxHeap extends BinaryHeap {
 
     public MaxHeap(int capacity){
