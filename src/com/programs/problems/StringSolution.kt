5c665725376752b5a036d81ee0dc79ea7566d745
anuj-dialpad <ajha@dialpad.com>
2020-03-25 22:25:16 +0530

    Problems: Check palindrome permutation

--- src/com/programs/problems/StringSolution.kt
+++ src/com/programs/problems/StringSolution.kt
@@ -4,8 +4,33 @@ import java.util.*
 
 
 fun main() {
-    println("Hello World!")
-    print(reorganizeString("aaabbcc"))
+    print(checkPalindromePermutation("aba&"))
+}
+
+/**
+ * Given a string check if any permutation of it can be palindrome.
+ * Ignore special characters
+ *
+ * Solution: String can only be a palindrome if it has every character appearing even number except any one character
+ * can appear odd number if string length is odd
+ */
+fun checkPalindromePermutation(str: String): Boolean {
+    val input = str.toLowerCase()
+    val arr = IntArray(26)
+    input.forEach {
+        if (it in 'a'..'z') {
+            arr[it - 'a']++
+        }
+    }
+    var oddFound = false
+    for (i in arr) {
+        if (i % 2 == 1) {
+            if (oddFound) return false
+            oddFound = true
+        }
+    }
+
+    return true
 }
 
 /**
@@ -16,26 +41,26 @@ fun reorganizeString(str: String): String {
     val len = str.length
     var count = IntArray(26) // used to decode the chars
     str.forEach {
-        count[it-'a']+=100 // add 100 for each time a char appears
+        count[it - 'a'] += 100 // add 100 for each time a char appears
     }
-    for(i in count.indices){
-        count[i]+=i
+    for (i in count.indices) {
+        count[i] += i
     }
     // count represent count*100+char
     Arrays.sort(count) ///sort the count array
     val ans = CharArray(len)
     var t = 1
-    count.forEach {total->
-         val cnt = total/100
-         if(cnt>(len+1)/2) return ""
-         if(cnt>0) {
-             val ch: Char = ('a' + total % 100)
-             for (i in 0 until cnt) {
-                 if (t >= len) t = 0
-                 ans[t] = ch
-                 t += 2
-             }
-         }
+    count.forEach { total ->
+        val cnt = total / 100
+        if (cnt > (len + 1) / 2) return ""
+        if (cnt > 0) {
+            val ch: Char = ('a' + total % 100)
+            for (i in 0 until cnt) {
+                if (t >= len) t = 0
+                ans[t] = ch
+                t += 2
+            }
+        }
 
     }
     return String(ans)
@@ -48,9 +73,9 @@ fun countPalindromes(s: String): Any? {
 
     val res = IntArray(1)
     res[0] = 0
-    for (i in 0..s.length){
-        countPalindromes(s, i,i, res)
-        countPalindromes(s, i,i+1,res)
+    for (i in 0..s.length) {
+        countPalindromes(s, i, i, res)
+        countPalindromes(s, i, i + 1, res)
     }
     return res[0]
 }
@@ -58,8 +83,8 @@ fun countPalindromes(s: String): Any? {
 fun countPalindromes(s: String, i: Int, j: Int, res: IntArray) {
     var left = i
     var right = j
-    while(left>=0 && right<s.length && s[left]==s[right]){
-        res[0]+=1
+    while (left >= 0 && right < s.length && s[left] == s[right]) {
+        res[0] += 1
         left--
         right++
     }
