26be3a040cf9ba6463bbd2e2a3bb596948b6183d
Anuj Kumar Jha <5445235+anuj-github@users.noreply.github.com>
2020-06-29 19:21:27 +0530

    Cycle graph unweighted directed (#248)
    * Problems: Detect cycle in directed unweighted graph
    
    * Problems: Detect cycle in directed unweighted graph
    
    * Problems: Detect cycle in directed unweighted graph
    
    Co-authored-by: anuj-dialpad <ajha@dialpad.com>

---
+++ src/com/programs/problems/graph/CourseSchedule.java
@@ -0,0 +1,67 @@
+package com.programs.problems.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseSchedule {
+
+    int total;
+    List<Integer>[] list;
+
+    public static void main(String args[]) {
+        CourseSchedule schedule = new CourseSchedule();
+        int total = 2;
+        int edges[][] = {{0, 1}, {1, 0}};
+        System.out.println(schedule.canFinish(total, edges));
+    }
+
+
+    /**
+     * https://leetcode.com/problems/course-schedule/
+     * Detect cycle in directed unweighted graph.
+     *  This approach uses depth first search
+     * @param numCourses
+     * @param edges
+     * @return
+     */
+    public boolean canFinish(int numCourses, int[][] edges) {
+        total = numCourses;
+        list = new List[total];
+        for (int i = 0; i < total; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            list[edge[0]].add(edge[1]);
+        }
+        boolean visited[] = new boolean[total];
+        boolean curr[] = new boolean[total];
+        for (int i = 0; i < total; i++) {
+            if (!visited[i]) {
+                if (!canFinish(i, visited, curr)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+
+    private boolean canFinish(int vertices, boolean[] visited, boolean[] curr) {
+        curr[vertices] = true;
+        visited[vertices] = true;
+        for (Integer node : list[vertices]) {
+            if (curr[node]) {
+                return false;
+            }
+            if (!visited[node]) {
+                if (!canFinish(node, visited, curr)) {
+                    return false;
+                }
+            }
+        }
+
+        curr[vertices] = false;
+        return true;
+    }
+}
