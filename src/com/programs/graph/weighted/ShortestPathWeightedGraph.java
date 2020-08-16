92446e80b1e4f9251eeec2cd3edb5943898b36b9
anuj-dialpad <ajha@dialpad.com>
2020-08-16 22:25:05 +0530

    Problems: LeetCode 743, network delay

--- src/com/programs/graph/weighted/ShortestPathWeightedGraph.java
+++ src/com/programs/graph/weighted/ShortestPathWeightedGraph.java
@@ -10,7 +10,7 @@ public class ShortestPathWeightedGraph {
     List<GraphNode>[] nodes;
 
     ShortestPathWeightedGraph(int size) {
-        vertices = size;
+        vertices = size+1;
         nodes = new LinkedList[vertices];
         for (int i = 0; i < vertices; i++) {
             nodes[i] = new LinkedList<>();
@@ -52,13 +52,8 @@ public class ShortestPathWeightedGraph {
        // System.out.println("Distance to " +dest +" is " + dist[dest]);
 
         // Print the calculated shortest distances
-        for (int i = 0; i < vertices; i++)
-        {
-            if (dist[i] == def)
-                System.out.println( i+" INF ");
-            else
-                System.out.println( i +" " + dist[i] + " ");
-        }
+        // Print the calculated shortest distances
+        System.out.println(Arrays.toString(dist));
 
     }
 
@@ -75,37 +70,48 @@ public class ShortestPathWeightedGraph {
 
     public  static void main(String[] args){
 
-        ShortestPathWeightedGraph g = new ShortestPathWeightedGraph(9);
-        g.addEdge(0, 1, 5);
-        g.addEdge(0, 6, 5);
-        g.addEdge(0, 2, 3);
-        g.addEdge(1, 3, 6);
-        g.addEdge(1, 8, 6);
-        g.addEdge(1, 7, 0);
+        ShortestPathWeightedGraph g = new ShortestPathWeightedGraph(5);
+        g.addEdge(1, 3, 68);
+        g.addEdge(1, 4, 20);
+        g.addEdge(4, 1, 65);
+        g.addEdge(3, 2, 74);
+        g.addEdge(2, 1, 44);
+        g.addEdge(3, 4, 61);
         //g.addEdge(7, 8, 0);
 
-        g.addEdge(1, 2, 2);
-        g.addEdge(1, 5, 9);
-        g.addEdge(2, 4, 4);
+        g.addEdge(4, 3, 68);
+        g.addEdge(3, 1, 26);
+        g.addEdge(5, 1, 60);
         //g.addEdge(2, 5, 2);
-        g.addEdge(2, 3, 2);
-        g.addEdge(3, 5, 1);
-        g.addEdge(6, 7, 1);
-        g.addEdge(6, 8, 11);
-        //g.addEdge(4, 5, -2);
+        g.addEdge(5, 3, 3);
+        g.addEdge(4, 5, 5);
+        g.addEdge(2, 5, 36);
+
+
+        g.addEdge(2, 3, 94);
+        g.addEdge(1, 2, 0);
+        g.addEdge(3, 5, 90);
+
+        g.addEdge(2, 4, 28);
+        g.addEdge(4, 2, 12);
+        g.addEdge(5, 4, 52);
         System.out.println("Following are shortest distances from source " + 1 );
         g.shortestPath(1);
         g.shortestPathUsingMinHeap(1);
     }
 
+    /**
+     * https://leetcode.com/problems/network-delay-time/
+     * Leetcode 743.
+     * We need to find shortest distance to each reacjable node from given source.
+     * And maximum among then will be our answer. If graph is not connected then answer should be -1 as we cant reach all the source
+     * This method only find shorted distance. we need to add check for whether we can reach everynode or not.
+     * to check this just check if any node's distance is Integer.MAX_VALUE
+     * @param src
+     */
     public void shortestPathUsingMinHeap(int src){
         PriorityQueue<GraphNode> pq = new PriorityQueue();
         pq.add(new GraphNode(src, 0));
-        for (int i = 0; i < vertices; i++) {
-            if (i != src) {
-                pq.add(new GraphNode(i, Integer.MAX_VALUE));
-            }
-        }
         boolean visited[] = new boolean[vertices];
         int distance[] = new int[vertices];
         Arrays.fill(distance, Integer.MAX_VALUE);
@@ -114,10 +120,9 @@ public class ShortestPathWeightedGraph {
             GraphNode parent = pq.poll();
             visited[parent.v] = true;
             for(GraphNode node: nodes[parent.v]){
-                GraphNode p = new GraphNode(node.v, distance[parent.v]+node.weight);
-                if(!visited[p.v] && distance[parent.v]!= Integer.MAX_VALUE && distance[node.v]>= distance[parent.v]+node.weight ){
+                if(!visited[node.v] && distance[node.v]>= distance[parent.v]+node.weight ){
+                    GraphNode p = new GraphNode(node.v, distance[parent.v]+node.weight);
                     distance[node.v] = distance[parent.v]+node.weight;
-                    pq.remove(p);
                     pq.offer(p);
                 }
             }
@@ -125,12 +130,7 @@ public class ShortestPathWeightedGraph {
 
         System.out.println("Distance using pq");
         // Print the calculated shortest distances
-        for (int i = 0; i < vertices; i++) {
-            if (distance[i] == Integer.MAX_VALUE)
-                System.out.println(i + " Not Reachable");
-            else
-                System.out.println(i + " " + distance[i] + " ");
-        }
+        System.out.println(Arrays.toString(distance));
     }
 
 }
