5f11f05a05ab273a48ec1afe145c613567d6c856
anuj-dialpad <ajha@dialpad.com>
2020-09-02 22:03:51 +0530

    Problems: Leetcode 96, Unique binary search tress

--- src/com/programs/problems/TreeProblems.java
+++ src/com/programs/problems/TreeProblems.java
@@ -29,6 +29,27 @@ public class TreeProblems {
         System.out.print(tp.findPath(node, 41));
     }
 
+    /**
+     * https://leetcode.com/problems/unique-binary-search-trees/
+     * Time complexity = N^2
+     * @param n
+     * @return
+     */
+    public int numTrees(int n) {
+        if(n<=2) return n;
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] =2;
+        for(int i =3;i<=n;i++){
+            for(int j=0;j<i;j++){
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
+
+
     int res;
 
     /**
